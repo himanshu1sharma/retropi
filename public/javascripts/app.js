df6c842d2c7c7b21e4a9b86b9e7a2d253846e2b3
Himanshu Sharma <hs@MacBook-Pro.local>
2014-12-23 12:16:34 +0000

    new design

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-
-$(document).on('mobileinit', function () {
-    $.mobile.pushStateEnabled = false;
-});
-
-});
 
 //var socket = io.connect('http://localhost:3000');
 //var socket = io.connect('http://app.himanshusharma.info');
@@ -26,61 +19,3 @@ function turnOn(pinNumber){
 function turnOff(pinNumber){
     socket.emit('turnOff', {pin : pinNumber});
 };
-
-socket.on('stateChange', function (data) {
-	pinState = data.Current;
-    //console.log(pinState);
-    setValues(data.Current);
-});
-
-function getState(callback){
-	$.get('/pinState/', function(roomList){
-        callback(roomList);
-
-        setTimeout(function () { 
-                setValues(roomList);  
-     },1000); 
-    });
-}
-
-function saveConfig(config) {
-
-    //console.log(config);
-
-    var obj = {};
-    obj.data = {};
-    obj.data.roomList = config;
-
-    for(var i=0 ; i < config.length; i++) {
-        try {
-            obj.data.roomList[i].switches = config[i].switches();
-        } catch(e) { obj.data.roomList[i].switches = config[i].switches;
-        }
-    }
-   console.log(obj);
-
-    socket.emit('saveSettings', obj);
-}
-
-function setValues(roomList){
-    console.log('Setting Pin States');
-    var switches;
-    for(var i=0; i<roomList.data.roomList.length ;i++){
-        try{
-         switches = roomList.data.roomList[i].switches();
-        }catch(e) {
-         //console.log(e);
-         switches = roomList.data.roomList[i].switches;
-        }
-        for (var j = 0; j < switches.length; j++) {
-            try {
-                      $(document).find('#button'+ switches[j].pin).val(switches[j].value).slider('refresh'); 
-                } catch(e) {
-                    //console.log(e);
-                    $(document).find('#button'+ switches[j].pin).slider();
-                    $(document).find('#button'+ switches[j].pin).val(switches[j].value).slider('refresh');
-            }
-        }
-    }
-}
-// roomList.data.roomList[i].
